b3ef9d75fc4472b08b3cb7dc4b49139bf28851bc
grandhappy <591965834@163.com>
2019-01-09 01:22:06 +0800

    first add

---
+++ src/main/java/com/sexgirl/App.java
@@ -0,0 +1,14 @@
+package com.sexgirl;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Configuration
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
